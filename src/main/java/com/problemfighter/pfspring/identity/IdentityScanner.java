06cd2b4ecfee4d769c8fe961a86a4bf42a0ba441
hmtmcse <hmtm.cse@gmail.com>
2021-01-10 22:53:36 +0600

    bismillah

---
+++ src/main/java/com/problemfighter/pfspring/identity/IdentityScanner.java
@@ -0,0 +1,8 @@
+package com.problemfighter.pfspring.identity;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.problemfighter.pfspring.identity.*"})
+public class IdentityScanner {
+}
