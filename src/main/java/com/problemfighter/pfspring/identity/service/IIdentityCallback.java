6e83c0b67cc123ba13f2e845d08ffab959391be7
hmtmcse <hmtm.cse@gmail.com>
2021-01-16 21:14:58 +0600

    add jwt refresh token

--- src/main/java/com/problemfighter/pfspring/identity/service/IIdentityCallback.java
+++ src/main/java/com/problemfighter/pfspring/identity/service/IIdentityCallback.java
@@ -2,7 +2,6 @@ package com.problemfighter.pfspring.identity.service;
 
 import com.problemfighter.pfspring.identity.model.dto.IAuthResponse;
 import com.problemfighter.pfspring.identity.model.entity.Identity;
-import com.problemfighter.pfspring.jwt.processor.JwtProcessor;
 import com.problemfighter.pfspring.jwt.service.JwtService;
 
 public interface IIdentityCallback {
