c1896f1d1a00c9bf0081e0ef16d89e826d82e977
hmtmcse <hmtm.cse@gmail.com>
2021-01-26 12:53:11 +0600

    Added Organization feature

--- src/main/java/com/problemfighter/pfspring/identity/model/dto/identity/IdentityDetailsDTO.java
+++ src/main/java/com/problemfighter/pfspring/identity/model/dto/identity/IdentityDetailsDTO.java
@@ -2,7 +2,8 @@ package com.problemfighter.pfspring.identity.model.dto.identity;
 
 public class IdentityDetailsDTO extends IdentityMasterDTO {
 
+    public String name;
     public String email;
     public String mobile;
-    public String recoverToken;
+
 }
