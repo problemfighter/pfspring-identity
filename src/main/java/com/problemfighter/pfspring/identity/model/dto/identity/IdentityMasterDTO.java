58c73692a09c1cc4851d0f90ac5c417fb9583b27
hmtmcse <hmtm.cse@gmail.com>
2021-02-04 07:46:41 +0600

    added multiple things

--- src/main/java/com/problemfighter/pfspring/identity/model/dto/identity/IdentityMasterDTO.java
+++ src/main/java/com/problemfighter/pfspring/identity/model/dto/identity/IdentityMasterDTO.java
@@ -1,9 +1,12 @@
 package com.problemfighter.pfspring.identity.model.dto.identity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.problemfighter.pfspring.jpacommon.model.dto.DTOCommon;
+import com.problemfighter.pfspring.jpacommon.model.dto.DTOCommonGetter;
 import com.problemfighter.pfspring.restapi.inter.model.RestDTO;
 
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 
 public class IdentityMasterDTO extends DTOCommon implements RestDTO {
